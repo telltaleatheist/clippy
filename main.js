aea2338dc4dc51a15cf633878ca717941ec617c7
telltaleatheist <telltaleatheist@gmail.com>
2025-04-07 12:20:34 -0400

    starting project, setting up framework

--- main.js
+++ main.js
@@ -1,29 +1,12 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const { execFile } = require('child_process');
+const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 600,
-    height: 400,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    width: 800,
+    height: 600,
   });
 
   win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow();
-});
-
-// Listen for download request from renderer
-ipcMain.handle('download-video', async (_, url) => {
-  return new Promise((resolve, reject) => {
-    execFile('./yt-dlp', [url], (error, stdout, stderr) => {
-      if (error) return reject(stderr);
-      resolve(stdout);
-    });
-  });
-});
+app.whenReady().then(createWindow);
--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  runYtDlp: (url) => ipcRenderer.invoke('download-video', url)
-});
